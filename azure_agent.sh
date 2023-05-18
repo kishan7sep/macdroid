842f5f2a4c03c31a6d078d20419f499b367e9573
kishan7sep <67074550+kishan7sep@users.noreply.github.com>
2023-05-18 16:59:43 +0530

    Create azure_agent.sh

---
+++ azure_agent.sh
@@ -0,0 +1,4 @@
+mkdir myagent && cd myagent
+curl https://vstsagentpackage.azureedge.net/agent/3.220.0/vsts-agent-osx-x64-3.220.0.tar.gz -o vsts-agent-osx-x64-3.220.0.tar.gz
+tar -zxvf vsts-agent-osx-x64-3.220.0.tar.gz
+echo "b5cmysmc4kcciambcpnk7vz4qa44vtx6up3xzoqtw2wzawaxle2a" > token.txt
